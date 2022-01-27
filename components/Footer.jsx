d33b2d433f87f58540eb0f127c86b60539afbdc4
Fatih <fatihce57@gmail.com>
2022-01-28 00:23:26 +0100

    footer with styling done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,31 @@
+import styles from '../styles/Footer.module.css'
+import Image from 'next/image'
+
 const Footer = () => {
-  return <div>
-      footer
-  </div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" alt="footer-image" layout="fill" objectFit="cover" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>YOU CAN FIND HIER. BEST PIZZA, BEST DELICIOUS!</h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND YOUR RESTAURANT</h1>
+          <p className={styles.text}>Centerstr. 1 <br /> 13000 Spandau <br /> Berlin</p>
+          <p className={styles.text}>Centerstr. 2 <br /> 12000 Mitte <br /> Berlin</p>
+          <p className={styles.text}>Centerstr. 3 <br /> 11000 Neuköln <br /> Berlin</p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            Mon-Sun <br /> 9:00 - 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Footer;

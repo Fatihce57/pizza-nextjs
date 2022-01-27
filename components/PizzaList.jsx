a01d5ce5dd17af59846126b891d6766410b29ab8
Fatih <fatihce57@gmail.com>
2022-01-27 15:35:11 +0100

    courosel added and styled

---
+++ components/PizzaList.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/PizzaList.modules.css'
+
+const PizzaList = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+export default Pizza(
+    List;
+

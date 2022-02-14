2e539f87f4e83ca2ccdac4d12e708cb21b2e4380
Fatih <fatihce57@gmail.com>
2022-02-14 23:38:29 +0100

    extras funtion added

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -7,6 +7,8 @@ const Product = ({ pizza }) => {
 
     const [price, setPrice] = useState(pizza.prices[0]);
     const [size, setSize] = useState(0);
+    const [quantity, setQuantity] = useState(1);
+    const [extras, setExtras] = useState([]);
 
     const changePrice = (number) => {
         setPrice(price + number);
@@ -22,11 +24,13 @@ const Product = ({ pizza }) => {
         const checked = e.target.checked;
 
         if (checked) {
-            changePrice(option.price)
+            changePrice(option.price);
+            setExtras((prev) => [...prev, option]);
         } else {
-            changePrice(-option.price)
+            changePrice(-option.price);
+            setExtras(extras.filter((extra) => extra._id !== option._id));
         }
-    }
+    };
 
     return (
         <div className={styles.container}>
@@ -71,7 +75,7 @@ const Product = ({ pizza }) => {
 
                 </div>
                 <div className={styles.add}>
-                    <input type="number" defaultValue={1} className={styles.quantity} />
+                    <input onChange={(e)=>setQuantity(e.target.value)}} type="number" defaultValue={1} className={styles.quantity} />
                     <button className={styles.button}>Add to Cart</button>
                 </div>
             </div>

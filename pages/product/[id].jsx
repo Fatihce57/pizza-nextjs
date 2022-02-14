27f4b267d70bb276b5a5fc2a716dc06d5c9b611f
Fatih <fatihce57@gmail.com>
2022-02-15 00:32:28 +0100

    navbar quantity funtion added

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import styles from '../../styles/Product.module.css'
 import Image from 'next/image'
 import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { addProduct } from '../../redux/cartSlice'
 
 const Product = ({ pizza }) => {
   const [price, setPrice] = useState(pizza.prices[0])
   const [size, setSize] = useState(0)
   const [quantity, setQuantity] = useState(1)
   const [extras, setExtras] = useState([])
+  const dispatch = useDispatch();
 
   const changePrice = (number) => {
     setPrice(price + number)
@@ -29,6 +32,10 @@ const Product = ({ pizza }) => {
       changePrice(-option.price)
       setExtras(extras.filter((extra) => extra._id !== option._id))
     }
+  }
+
+  const handleClick = () => {
+    dispatch(addProduct({ ...pizza, extras, price, quantity }))
   }
 
   return (
@@ -84,7 +91,7 @@ const Product = ({ pizza }) => {
             defaultValue={1}
             className={styles.quantity}
           />
-          <button className={styles.button}>Add to Cart</button>
+          <button className={styles.button} onClick={handleClick}>Add to Cart</button>
         </div>
       </div>
     </div>

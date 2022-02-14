456b72bb4dcd3916fbfb2ff2ec02d538f94d9f38
Fatih <fatihce57@gmail.com>
2022-02-15 00:56:52 +0100

    cart total function added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Navbar.module.css"
 import Image from "next/image"
 import { useSelector } from "react-redux"
+import Link from 'next/link'
 
 const Navbar = () => {
 
@@ -28,12 +29,14 @@ const Navbar = () => {
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
-      <div className={styles.item}>
-        <div className={styles.cart}>
-          <Image src="/img/cart.png" alt="logo" width="30" height="30" />
-          <div className={styles.counter}>{quantity}</div>
+      <Link href="/cart" passHref>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" alt="logo" width="30" height="30" />
+            <div className={styles.counter}>{quantity}</div>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 };
